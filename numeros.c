f202e8c5f1558e08b1b571495b51523c863e45cd
Oscarolo13 <osctisser@alu.edu.gva.es>
2024-12-03 13:44:09 +0100

    Add files via upload

---
+++ numeros.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main (){
+    int n;
+
+    printf("Introdueix un numero: ");
+    scanf("%d",&n);
+    if(n == 1) printf("U\n");
+    if(n == 2) printf("Dos\n");
+    if(n == 3) printf("Tres\n");
+    if(n == 4) printf("Quatre\n");
+    if(n == 5) printf("Cinc\n");
+    return;
+}
